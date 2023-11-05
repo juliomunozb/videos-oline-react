9fabb948f6f31fd734040c08058b43738b59a6b9
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-04 22:47:47 -0500

    Configuración punto de entra principal de la app e instalación de babel

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  render(<h1>Hol</h1>);
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+//Archivo de punto de entrada. Primer archivo que será detectado
+ReactDOM.render(<App />, document.getElementById("app"));
