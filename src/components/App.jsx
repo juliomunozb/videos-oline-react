732615175ca67828762347f9311cddd6c5610550
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-07 10:37:14 -0500

    Configución para agregar hash a los archivos de producción

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
-const App = () => <h1>Hol</h1>;
+import "../assets/styles/App.scss";
+const App = () => <h1>Hola</h1>;
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require("path"); //Depedencia nativa de node
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const rulesForStyles = {
+  test: /\.(c|sc|sa)ss$/,
+  use: [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    "css-loader",
+    "sass-loader",
+  ],
+};
 const rulesForJavaScript = {
   test: /\.(js|jsx)$/,
   exclude: /node_modules/,
@@ -17,35 +28,45 @@ const rulesForHtml = {
     },
   ],
 };
-const rules = [rulesForJavaScript, rulesForHtml];
+const rules = [rulesForJavaScript, rulesForHtml, rulesForStyles];
 
-module.exports = {
-  //Punto de entrada de la app
-  entry: "./src/index.js",
-  //directorio donde se empaquetarán los archivos
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  //Extensiones que debe resolver para el paquete
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  //Cargando los loaders
-  module: {
-    rules,
-  },
-  //Añadir funcionalidades adicionales a webpack
-  plugins: [
-    // Crear automáticamente el archivo index.html en el paquete generado
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-    }),
-  ],
-  //Parametros para la configuración del server de desarrollo
-  devServer: {
-    open: true, //Abrimos el navegador
-    port: 30002,
-  },
+module.exports = (env, argv) => {
+  //Obteniendo si el deploy es desarrollo o producción
+  const { mode } = argv;
+  const isProduction = mode === "production";
+  return {
+    //Punto de entrada de la app
+    entry: "./src/index.js",
+    //directorio donde se empaquetarán los archivos
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: isProduction ? "[name].[contenthash].js" : "main.js",
+    },
+    //Extensiones que debe resolver para el paquete
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    //Cargando los loaders
+    module: {
+      rules,
+    },
+    //Añadir funcionalidades adicionales a webpack
+    plugins: [
+      // Crear automáticamente el archivo index.html en el paquete generado
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProduction
+          ? "./assets/[name].[contenthash].css"
+          : "./assets/[name].css",
+      }),
+    ],
+    //Parametros para la configuración del server de desarrollo
+    devServer: {
+      open: true, //Abrimos el navegador
+      port: 30002,
+    },
+  };
 };
