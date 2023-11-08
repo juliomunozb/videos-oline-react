8d5f1aeab6c3425818ff38c39b9c1cdfa8982ee7
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-08 15:34:25 -0500

    creando ruta para NotFound y componente NotFound

---
+++ src/containers/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/styles/components/NotFound.scss'
+import '../assets/styles/components/_animation.scss'
+const NotFound = () => (
+  <section className='error'>
+    <section className='error__container'>
+      <p className='error__container--code animation rotate'>404</p>
+      <p className='error__container--description'>
+        Página no encontrada
+      </p>
+    </section>
+  </section>
+)
+export default NotFound
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../containers/Home'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
+import NotFound from '../containers/NotFound'
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<Home />} />
       <Route exact path='/login' element={<Login />} />
       <Route exact path='/Register' element={<Register />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 )
