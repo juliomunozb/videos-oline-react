479a1e555e69e60fcf108224edb15beaf4a9fe59
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-09 12:43:45 -0500

    Instalacion de redux, creación estructura inicial para trabajar con redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './routes/App'
 // Archivo de punto de entrada. Primer archivo que será detectado
 
 const container = document.getElementById('app')
 const root = createRoot(container)
-root.render(<App />)
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+)
