d9c96b541f586c2de0b734e5862044f10308a5c2
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-10 15:49:42 -0500

    Funcionalidad para eliminar video de favoritos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
-// Action para guardar elementos en favoritos
+// Action para guardar elementos en favoritos - - myList
 export const setFavorite = (payload) => (
   {
     type: 'SET_FAVORITE',
+    payload
+  }
+)
+// Accion para aliminar elementos de Favoritos - myList
+export const deleteFavorite = (payload) => (
+  {
+    type: 'DELETE_FAVORITE',
     payload
   }
 )
--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setFavorite } from '../actions'
+import { setFavorite, deleteFavorite } from '../actions'
 import PropTypes from 'prop-types'
 import '../assets/styles/components/CarouselItem.scss'
 import iconPlay from '../assets/static/play-icon.png'
 import iconPlus from '../assets/static/plus-icon.png'
+import iconDelete from '../assets/static/delete-icon.png'
 
 const CarouselItem = (props) => {
-  const { cover, title, year, contentRating, duration } = props
+  const { id, cover, title, year, contentRating, duration } = props
 
   const handleSetFavorite = () => {
-    props.setFavorite({ cover, title, year, contentRating, duration })
+    props.setFavorite({ id, cover, title, year, contentRating, duration })
+  }
+
+  const handleDeleteFavorite = (idItem) => {
+    props.deleteFavorite(idItem)
   }
 
   return (
@@ -29,6 +34,12 @@ const CarouselItem = (props) => {
             alt='pluss'
             onClick={handleSetFavorite}
           />
+          <img
+            className='carousel-item__details--img'
+            src={iconDelete}
+            alt='delete'
+            onClick={() => handleDeleteFavorite(id)}
+          />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
         <p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration}`}</p>
@@ -46,6 +57,8 @@ CarouselItem.propTypes = {
 }
 
 const mapDispatchToProps = {
-  setFavorite
+  setFavorite,
+  deleteFavorite
+
 }
 export default connect(null, mapDispatchToProps)(CarouselItem)
